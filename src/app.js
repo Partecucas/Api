79e395038473d9dcd4cac82987ffd5f3abbc1478
Juan david <jdlopez2013@misena.edu.co>
2023-05-09 19:00:44 -0500

    ajustes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,45 @@
-import express from 'express'
-import { pool } from './db.js'
-import {PORT} from './config.js'
+import express from 'express';
+import cors from 'cors';
+import { pool } from './db.js';
+import { PORT } from './config.js';
 
-const app = express()
+const app = express();
+
+app.use(cors());
+
+app.use(express.json()); // Agregamos este middleware para poder procesar los datos del cuerpo en formato JSON
 
 app.get('/', async (req, res) => {
-  const [rows] = await pool.query('SELECT * FROM users')
-  res.json(rows)
-})
+  const [rows] = await pool.query('SELECT * FROM users');
+  res.json(rows);
+});
 
 app.get('/user', async (req, res) => {
-  const [result] = await pool.query(`SELECT * FRO; users`);
-  res.json(result[0])
-})
+  const [results] = await pool.query('SELECT * FROM users');
+  res.json(results);
+});
 
-app.listen(PORT)
-console.log('Server on port', PORT)
+
+
+// Agregamos la función para insertar un nuevo usuario con un nombre y un ID
+// app.post('/iuser', async (req, res) => {
+//   const { id,name } = req.body; // Obtenemos los datos del cuerpo de la solicitud
+//   const result = await pool.query(`INSERT INTO users(id, name) VALUES ("${id}", "${name}")`);
+//   res.json(result);
+// });
+
+// app.post('/asistencia', async (req, res) => {
+//   const { id,fecha, hora, documento, placa, vehiculo } = req.body; // Obtenemos los datos del cuerpo de la solicitud
+//   const result = await pool.query(`INSERT INTO registroe (id,fecha, hora, documento, placa, vehiculo) VALUES ("${id}","${fecha}","${hora}", "${documento}", "${placa}", "${vehiculo}")`);
+//   res.json(result);
+// });
+
+// app.get('/gasistencia', async (req, res) => {
+//   const [results] = await pool.query('SELECT * FROM registro');
+//   res.json(results);
+// });
+
+
+
+app.listen(PORT);
+console.log('Server on port', PORT);
